884352d29a44678f671ac5cf57b05d92b93b362e
Rashid20071601 <153351715+Rashid20071601@users.noreply.github.com>
2025-01-11 09:49:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Импорт библиотек
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, HTTPException, Path
 from typing import Annotated
 
 # Создаем экземпляр приложения FastAPI
@@ -21,15 +21,23 @@ async def registered_user(username: str, age: int) -> str:
     return f'User {current_id} is registered!'
 
 
-@app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str, username: str, age: int) -> str:
+async def update_user(
+    user_id: str,
+    username: str,
+    age: Annotated[int, Path(gt=0)]
+) -> str:
+    # Проверяем, существует ли пользователь с указанным ID
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
     # Обновляем данные существующего пользователя
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'The user {user_id} is updated!'
 
 
-@app.delete('/user/{user_id}')
 async def deleted_user(user_id: str) -> str:
+    # Проверяем, существует ли пользователь с указанным ID
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
     # Удаляем пользователя по его ID
     users.pop(user_id)
-    return f'The user {user_id} is deleted'
+    return f'The user {user_id} is deleted!'
